c35533806877168a78f6d8a771e2e0ddbae336ea
tacosman769 <shunfujit@yahoo.co.jp>
2020-12-11 10:24:51 +0900

    写真修正

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.gif'
+import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.png'
 import pic03 from '../images/pic03.png'
 
